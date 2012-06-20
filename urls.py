e4f1caa0e3f3625f858c6e5f32ab75a2e90bc26e
karthikabinav <karthikabinavs@gmail.com>
2012-06-20 23:56:49 +0530

    Added the APIs app

--- urls.py
+++ urls.py
@@ -1,17 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'API.views.home', name='home'),
-    # url(r'^API/', include('API.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+        
+    url(r'^events/(?P<params>.*)/$', 'API.APIs.views.EventHandler'),
+    url(r'^users/(?P<params>.*)/$', 'API.APIs.views.UserHandler'),
+    url(r'^sessions/(?P<params>.*)/$', 'API.APIs.views.SessionsHandler'),
 )
