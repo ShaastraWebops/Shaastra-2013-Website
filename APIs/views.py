9d465ee5089038bc85a48d24f018823360ecdc3d
karthikabinav <karthikabinavs@gmail.com>
2012-06-21 00:13:52 +0530

    A Basic views returning a json response

--- APIs/views.py
+++ APIs/views.py
@@ -1,1 +1,38 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template.context import Context, RequestContext
+import re,sys
+import json
+
+#Test function to test the POST APIs
+def test(request):
+    return render_to_response('test.html', locals(), context_instance=RequestContext(request))
+
+def EventHandler(request,params):
+    rendered = json.dumps({"status":"invalid code"})
+    if request.method == "POST":
+        rendered = json.dumps({"method":"POST","views":"event"})        
+    else:
+        rendered = json.dumps({"method":"GET","views":"event"}) 
+        
+    return HttpResponse(rendered, mimetype='application/json')
+ 
+def UserHandler(request,params):
+    rendered = json.dumps({"status":"invalid code"})
+    if request.method == "POST":
+        rendered = json.dumps({"method":"POST","views":"users"})        
+    else:
+        rendered = json.dumps({"method":"GET","views":"users"}) 
+        
+    return HttpResponse(rendered, mimetype='application/json')
+
+def SessionsHandler(request,params):
+    rendered = json.dumps({"status":"invalid code"})
+    if request.method == "POST":
+        rendered = json.dumps({"method":"POST","views":"sessions"})        
+    else:
+        rendered = json.dumps({"method":"GET","views":"sessions"}) 
+        
+    return HttpResponse(rendered, mimetype='application/json')
+
+       
--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
         
+    url(r'^test/$', 'API.APIs.views.test'),
     url(r'^events/(?P<params>.*)/$', 'API.APIs.views.EventHandler'),
     url(r'^users/(?P<params>.*)/$', 'API.APIs.views.UserHandler'),
     url(r'^sessions/(?P<params>.*)/$', 'API.APIs.views.SessionsHandler'),
