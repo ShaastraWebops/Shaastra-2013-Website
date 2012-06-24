28ffa63213cadf40c30f53a9c703e76ee2064d28
S.R.Manikandasriram <mani@mani-Inspiron-N5110.(none)>
2012-06-24 20:50:17 +0530

    Core completed

--- admin/ajax.py
+++ admin/ajax.py
@@ -34,9 +34,9 @@ def add_edit_group(request,form="",id=0):
 def updateSummary(request):
     dajax = Dajax()
     dajax.assign("#summary",'innerHTML',"<table border='1'><thead><tr><th>S.No</th><th>Group Name</th><th>Cores</th></tr></thead><tbody id='groups'>")
-    groups=Group.objects.order_by('id').all()
+    groups=Group.objects.order_by('id').all()[1:]
     for g in groups:
-        dajax.append("#groups",'innerHTML',"<tr><td>"+str(g.id)+"</td><td onclick=\'displayGroup("+str(g.id)+");\' class='grps' id="+g.name+"><a href=#>"+g.name+"</a></td><td id="+str(g.id)+"></td></tr>")
+        dajax.append("#groups",'innerHTML',"<tr><td>"+str(g.id-1)+"</td><td onclick=\'displayGroup("+str(g.id)+");\' class='grps' id="+g.name+"><a href=#>"+g.name+"</a></td><td id="+str(g.id)+"></td></tr>")
         cores=User.objects.filter(groups__name=g.name)
         for c in cores:
             dajax.append("#"+str(g.id),'innerHTML',"<li onclick=\'displayCore("+str(c.id)+");\' class='cores' id="+str(c.username)+"><a href=#>"+str(c)+"</a>")
@@ -67,27 +67,24 @@ def add_edit_core(request,form="",id=0):
         return dajax.json()
     if id:
         core_form = AddCoreForm(form, instance=User.objects.get(id=id))
+        if core_form.is_valid():
+            core_form.save()
+            dajax.assign("#space",'innerHTML',"")
+        else:
+            template = loader.get_template('ajax/admin/editcore.html')
+            html=template.render(RequestContext(request,locals()))
+            dajax.assign("#space",'innerHTML',html)
     else:
         core_form = AddCoreForm(form)
-    if core_form.is_valid():
-        data = core_form.cleaned_data
-        core_user=User(first_name = data['first_name'], last_name=data['last_name'], username= data['username'], email = data['email'])
-        core_user.set_password(data['password'])
-        core_user.save()
-        grp=Group.objects.get(id=form['group'])
-        grp.user_set.add(core_user)
-        core_user.save()
-        core_profile = UserProfile(
-                user = core_user,
-                gender = data['gender'],
-                age = data['age'],
-                mobile_number = data['mobile_number'],
-                is_core=True,
-                )
-        core_profile.save()
-        dajax.assign("#space",'innerHTML',"")
-    else:
-        dajax.prepend("#space",'innerHTML',"Enter valid details")
+        if core_form.is_valid():
+            core=core_form.save()
+            core.set_password("default")
+            core.save()
+            dajax.assign("#space",'innerHTML',"")
+        else:
+            template = loader.get_template('ajax/admin/addcore.html')
+            html=template.render(RequestContext(request,locals()))
+            dajax.assign("#space",'innerHTML',html)
     dajax.script("updateSummary();")
     return dajax.json()
 
