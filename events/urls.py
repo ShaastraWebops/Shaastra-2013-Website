de0fc944b997cea366d5b34ddabea57be94e52a6
Varshaa Naganathan <varshaan@gmail.com>
2012-06-28 21:37:03 +0530

    Added comments in admin and core apps

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from events.views import *
 
 urlpatterns = patterns('',
-    url(r'^/$', CoordDashboard()),
+    url(r'^$', CoordDashboard()),
     url(r'^tabfile/$', TabFileSubmit()),
 )
 
