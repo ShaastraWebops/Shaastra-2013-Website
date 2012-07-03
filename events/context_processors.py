10bc56e56f4eea2cff6af50c3c1acf944ce234e1
Abhishek Saini <abhishek0saini@gmail.com>
2012-07-03 06:01:13 +0530

    Coords view of submissions
    with the help of jquery DataTable plugin, coords can now view and sort submissions by participant name, read, selected, interesting, score.

--- events/context_processors.py
+++
@@ -1,4 +0,0 @@
-# instead of importing settings in each function, and then calling {{settings.PROJECT_DIR_NAME}} in templates, now you can directly write {{pro_dir}} and get the same thing. Make sure this function is included in your TEMPLATE_CONTEXT_PROCESSORS in your local settings.
-def project_dir_name(request):
-    from django.conf import settings
-    return {'pro_dir':settings.PROJECT_DIR_NAME}
