cfeaa2cd23b11678f8a641ea7b12303382f42e7d
Shaastra <webops@shaastra.org>
2012-07-04 08:54:55 -0500

    fb and forum integration

--- users/fb_views.py
+++ users/fb_views.py
@@ -1,12 +1,15 @@
 import urllib, cgi,json
 
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
-from django.contrib.auth import login as auth_login, authenticate
-from django.core.urlresolvers import reverse
+from django.shortcuts import *
 from django.conf import settings
-from users.forms import BaseUserForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate
+from django.core.urlresolvers import reverse
+from users.forms import AddUserForm, FacebookUserForm
 from users.models import UserProfile
+from django.conf import settings
+
 
 def login(request):
     """ First step of process, redirects user to facebook, which redirects to authentication_callback. """
@@ -14,8 +17,9 @@ def login(request):
     args = {
         'client_id': settings.FACEBOOK_APP_ID,
         'scope': settings.FACEBOOK_SCOPE,
-        'redirect_uri': 'http://127.0.0.1:8000/user/facebook/authentication_callback',
+        'redirect_uri': request.build_absolute_uri(settings.SITE_URL+'user/facebook/authentication_callback/'),
     }
+
     return HttpResponseRedirect('https://www.facebook.com/dialog/oauth?' + urllib.urlencode(args))
 
 def authentication_callback(request):
@@ -27,13 +31,12 @@ def authentication_callback(request):
     args = {
         'client_id': settings.FACEBOOK_APP_ID,
         'client_secret': settings.FACEBOOK_APP_SECRET,
-        'redirect_uri': 'http://127.0.0.1:8000/user/facebook/authentication_callback',
+        'redirect_uri': request.build_absolute_uri(settings.SITE_URL+'user/facebook/authentication_callback/'),
         'code': code,
     }
 
     # Get a legit access token
-    a='https://graph.facebook.com/oauth/access_token?' + urllib.urlencode(args)
-    target = urllib.urlopen(a).read()
+    target = urllib.urlopen('https://graph.facebook.com/oauth/access_token?' + urllib.urlencode(args)).read()
     response = cgi.parse_qs(target)
     access_token = response['access_token'][-1]
 
@@ -45,25 +48,26 @@ def authentication_callback(request):
 
     try:
         # Try and find existing user
-        fb_user = UserProfile.objects.get(facebook_id=fb_profile['id'])
-        user=authenticate(username=fb_user.user.email,password="default")
+        fb_user = UserProfile.objects.get(facebook_id=str(fb_profile['id']))
+        user=authenticate(username=fb_user.user,password="default")
         if user is not None:
             auth_login(request,user)
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect(settings.SITE_URL)
+
     except UserProfile.DoesNotExist:
         # No existing user
         # Not all users have usernames
-#        username = fb_profile.get('username', fb_profile['email'].split('@')[0])
-        email=fb_profile['email']
+        username = fb_profile.get('username', fb_profile['email'].split('@')[0])
+        email=fb_profile['email']            
         first_name = fb_profile['first_name']
         last_name = fb_profile['last_name']
-        facebook_id=fb_profile['id']
+        facebook_id=str(fb_profile['id'])
         password="default"
         password_again="default"
         if fb_profile['gender'] == "female" :
             gender='F'
         else:
             gender='M'
-        form = BaseUserForm(initial=locals())
+        post_url = settings.SITE_URL+'user/register_fb/'
+        form = FacebookUserForm(initial=locals())
     return render_to_response('users/register.html',locals(), context_instance=RequestContext(request))    
-
