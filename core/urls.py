1e2ac00c3740cf5a2e8d301228ab5e296b3c171b
S.R.Manikandasriram <mani@mani-Inspiron-N5110.(none)>
2012-07-07 22:27:41 +0530

    hash frags for core dashboard

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,10 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from core.views import *
 
-urlpatterns = patterns('',  
+urlpatterns = patterns('',
+    url(r'^addevent/', addevent, name='addevent'),
+    url(r'^editevent/(?P<id>\d+)', editevent, name='editevent'),
+    url(r'^addcoord/', addcoord, name='addcoord'),
+    url(r'^editcoord/(?P<id>\d+)', editcoord, name='editcoord'),
     url(r'^$', home, name = 'home'),
 )
--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template.context import Context, RequestContext
 from django.conf import settings
+from core.forms import *
 
 @login_required(login_url=settings.SITE_URL + 'user/login/')
 def home(request):
@@ -14,5 +15,43 @@ def home(request):
         return HttpResponseRedirect(settings.SITE_URL)
     return render_to_response('core/home.html', locals(), context_instance = RequestContext(request))
 
+@login_required(login_url=settings.SITE_URL + 'user/login/')
+def addevent(request):
+    """
+        This is the home page view of the superuser
+    """
+    if request.user.get_profile().is_core is False :
+        return HttpResponseRedirect(settings.SITE_URL)
+    event_form=AddEventForm()
+    return render_to_response('ajax/core/addevent.html', locals(), context_instance = RequestContext(request))
 
+@login_required(login_url=settings.SITE_URL + 'user/login/')
+def editevent(request,id=0):
+    """
+        This is the home page view of the superuser
+    """
+    if request.user.get_profile().is_core is False :
+        return HttpResponseRedirect(settings.SITE_URL)
+    event_form=AddEventForm(instance=Event.objects.get(id=id))
+    return render_to_response('ajax/core/editevent.html', locals(), context_instance = RequestContext(request))
+
+@login_required(login_url=settings.SITE_URL + 'user/login/')
+def addcoord(request):
+    """
+        This is the home page view of the superuser
+    """
+    if request.user.get_profile().is_core is False :
+        return HttpResponseRedirect(settings.SITE_URL)
+    coord_form=AddCoordForm()
+    return render_to_response('ajax/core/addcoord.html', locals(), context_instance = RequestContext(request))
+
+@login_required(login_url=settings.SITE_URL + 'user/login/')
+def editcoord(request,id=0):
+    """
+        This is the home page view of the superuser
+    """
+    if request.user.get_profile().is_core is False :
+        return HttpResponseRedirect(settings.SITE_URL)
+    coord_form=AddCoordForm(instance=User.objects.get(id=id))
+    return render_to_response('ajax/core/editcoord.html', locals(), context_instance = RequestContext(request))
 
