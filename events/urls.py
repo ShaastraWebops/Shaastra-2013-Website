ad53e0004fd795913c5bd195570bdfb9a02be2e7
S.R.Manikandasriram <mani@mani-Inspiron-N5110.(none)>
2012-07-12 19:19:49 +0530

    hash frags for coord dashboard

--- events/urls.py
+++ events/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls.defaults import patterns, include, url
 from events.views import *
 
 urlpatterns = patterns('',
-    url(r'^$', CoordDashboard()),
+    url(r'^customtabs/', CustomTabs()),
+    url(r'^questions/', QuestionsTab()),
+    url(r'^mobapp/$', MobAppTab()),
     url(r'^tabfile/$', TabFileSubmit()),
+    url(r'^$', CoordDashboard()),
 )
 
 #these urls will be imported by the root url.
