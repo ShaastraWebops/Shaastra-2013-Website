61e8008418eab5390613932f965be823d545d519
Abhishek Saini <abhishek0saini@gmail.com>
2012-07-20 19:14:01 +0530

    participants can now register/answer questions.

--- submissions/urls.py
+++ submissions/urls.py
@@ -4,5 +4,7 @@ from submissions.views import *
 
 urlpatterns = patterns('',
     url(r'^$', all_submissions, name = 'sub_home'),
-    url(r'^(?P<event_id>\d)/$', event_submission, name = 'sub_event'),
+    url(r'^(?P<event_id>\d+)/$', event_submission, name = 'sub_event'),
+    url(r'^(?P<eve_id>\d+)/subques/(?P<q_id>\d+)/$', save_edit_subjective, name = 'sub'),
+    url(r'^(?P<eve_id>\d+)/mcques/(?P<q_id>\d+)/$', save_edit_mcq, name = 'mcq'),
 )
