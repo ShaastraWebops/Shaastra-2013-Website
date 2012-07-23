91680ad186dc4738bb74cdd6a41f7bab0b041c28
Varshaa Naganathan <varshaan@gmail.com>
2012-07-23 21:05:39 +0530

    Updates displayed on main-site

--- events/views.py
+++ events/views.py
@@ -3,11 +3,14 @@ from django.template.context import Context, RequestContext
 from django.shortcuts import render_to_response
 from django.conf import settings
 from events.models import *
+from operator import attrgetter
 
 # Create your views here.
 
 def events(request, event_id):
     event = Event.objects.get(id = event_id)
+    initial = Update.objects.all()
+    update = sorted(initial, key=attrgetter('id'), reverse=True)
     tab_set = event.tab_set.all()
     return render_to_response('events/events.html',locals(), context_instance= RequestContext(request))
 
