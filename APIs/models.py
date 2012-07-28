027d13c3c9bada39b0eb41785c7e344a366df9a5
karthikabinav <karthikabinavs@gmail.com>
2012-07-28 16:41:09 +0530

    Changed the name of tables

--- APIs/models.py
+++ APIs/models.py
@@ -26,12 +26,21 @@ class Event(models.Model):
     def __unicode__(self):
         return '%s' % self.title
     
+    class Meta:
+        db_table='events_event'
+
 class Update(models.Model):
     description = models.TextField()
     category = models.CharField(max_length = 15, choices = CATEGORY, help_text='You can add 4 updates and 1 announcement. Mark as announcement only if the information is of highest importance')
     event = models.ForeignKey(Event, null=True, blank=True)
     expired = models.BooleanField(default=False, help_text='Mark an update/announcement as expired if it is no longer relevant or you have more than 4 updates (or 1 announcement) ')
 
+    class Meta:
+        db_table = 'events_update'
+
 class MobAppTab(models.Model):
     event = models.OneToOneField(Event, blank = True, null = True)
     text = models.TextField()
+
+    class Meta:
+        db_table = 'events_mobapptab'
