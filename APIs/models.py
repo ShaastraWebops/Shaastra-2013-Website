1846ed6e2b2b4e991f5292a592e960f9228b33d1
karthikabinav <karthikabinavs@gmail.com>
2012-07-28 15:36:02 +0530

    Added the querying of events table

--- APIs/models.py
+++ APIs/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+EVENT_CATEGORIES = (
+	("Aerofest", "Aerofest"),
+	("Coding", "Coding"),
+	("Design and Build", "Design and Build"),
+	("Involve", "Involve"),
+	("Quizzes", "Quizzes"),
+	("Online", "Online"),
+	("Department Flagship", "Department Flagship"),
+	("Spotlight", "Spotlight"),
+	("Workshops", "Workshops"),
+    ("Others", "Others"),
+)
+
+CATEGORY = (
+    ("Update", "Update"),
+    ("Announcement", "Announcement"),
+)
+
+class Event(models.Model):
+    title = models.CharField(max_length = 30)
+    category = models.CharField(max_length=50,choices= EVENT_CATEGORIES)
+    
+    def __unicode__(self):
+        return '%s' % self.title
+    
+class Update(models.Model):
+    description = models.TextField()
+    category = models.CharField(max_length = 15, choices = CATEGORY, help_text='You can add 4 updates and 1 announcement. Mark as announcement only if the information is of highest importance')
+    event = models.ForeignKey(Event, null=True, blank=True)
+    expired = models.BooleanField(default=False, help_text='Mark an update/announcement as expired if it is no longer relevant or you have more than 4 updates (or 1 announcement) ')
+
+class MobAppTab(models.Model):
+    event = models.OneToOneField(Event, blank = True, null = True)
+    text = models.TextField()
--- APIs/views.py
+++ APIs/views.py
@@ -3,17 +3,46 @@ from django.shortcuts import render_to_response
 from django.template.context import Context, RequestContext
 import re,sys
 import json
+from APIs.models import *
 
 #Test function to test the POST APIs
 def test(request):
     return render_to_response('test.html', locals(), context_instance=RequestContext(request))
 
-def EventHandler(request,params):
+def EventHandler(request,params=None):
     rendered = json.dumps({"status":"invalid code"})
     if request.method == "POST":
         rendered = json.dumps({"method":"POST","views":"event"})        
     else:
-        rendered = json.dumps({"method":"GET","views":"event"}) 
+        rendered = dict()
+        status = 200
+        if params == None:
+            rendered['events'] = list()
+            try:
+                events = Event.objects.all()
+                for event in events:
+                    rendered['events'].append({'title':event.title,'id':event.pk,'category':event.category})
+            except:
+                status = 500
+
+        else:
+            try:
+                mobopsTab = MobAppTab.objects.get(event = params)
+                updatesObject = Update.objects.filter(event = params,expired = False)
+                updates = list()
+                announcements = list()
+                for update in updatesObject:
+                    if update.category == "Update":
+                        updates.append(update.description)
+                    else:
+                        announcements.append(update.description)
+
+                rendered = {'text':mobopsTab.text,'updates':updates,'announcements':announcements}
+            except:
+                status = 500
+        
+        rendered['status'] = status
+        rendered = json.dumps(rendered) 
         
     return HttpResponse(rendered, mimetype='application/json')
  
