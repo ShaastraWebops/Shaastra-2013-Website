356a1c321b49b7df4721b5c1a3f2849db1a138f2
S.R.Manikandasriram <srmanikandasriram@gmail.com>
2012-08-11 22:15:35 +0530

    hash frags for sampark

--- events/views.py
+++ events/views.py
@@ -12,14 +12,15 @@ from operator import attrgetter
 
 def events(request, event_name):
     event_name = event_name.replace('-', ' ')
+    if event_name == 'sampark/' :
+        return sampark(request)
     event = Event.objects.get(title=event_name)
     initial_updates = Update.objects.filter(category = 'Update')
     updates = sorted(initial_updates, key=attrgetter('id'), reverse=True)
     initial_announcements = Update.objects.filter(category = 'Announcement')
     announcements = sorted(initial_announcements, key=attrgetter('id'), reverse=True)
     tab_set = event.tab_set.all()
-    return render_to_response('events/events.html', locals(),
-                              context_instance=RequestContext(request))
+    return render_to_response('events/events.html', locals(), context_instance=RequestContext(request))
 
 
 def tabs(request, event_name, tab_name):
@@ -29,10 +30,12 @@ def tabs(request, event_name, tab_name):
     tab_set = event.tab_set.all()
     tab = tab_set.get(title=tab_name)
     file_set = tab.tabfile_set.all()
-    return render_to_response('events/tabs.html', locals(),
-                              context_instance=RequestContext(request))
+    return render_to_response('events/tabs.html', locals(), context_instance=RequestContext(request))
 
 def sampark(request):
+#    if path:
+#        assert False
+#        return HttpResponseRedirect(settings.SITE_URL+path)
     bengaluruevents = []
     hyderabadevents = []
     puneevents = []
@@ -45,6 +48,5 @@ def sampark(request):
             hyderabadevents.append(e)
         if e.title.split('_')[0] == 'P':
             puneevents.append(e)
-    return render_to_response('events/sampark_home.html', locals(),
-                              context_instance=RequestContext(request))
+    return render_to_response('events/sampark_home.html', locals(), context_instance=RequestContext(request))
         
