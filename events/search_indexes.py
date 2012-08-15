5250efd323fd50e219b15300de482a0666d060bd
Swapnil Basak <swapnilbasak@gmail.com>
2012-08-15 22:16:08 +0530

    Fixed event urls

--- events/search_indexes.py
+++ events/search_indexes.py
@@ -1,6 +1,7 @@
 from haystack.indexes import *
 from haystack import site
 from events.models import *
+from django.template.defaultfilters import slugify
 
 class NoteIndexTabs(SearchIndex):
     text = CharField(document=True, use_template=True)
@@ -14,12 +15,20 @@ class NoteIndexTabs(SearchIndex):
                 return obj.event.title
         except:
             pass
+        
             
 class NoteIndexEvents(SearchIndex):
     text = CharField(document=True, use_template=True)
     title = EdgeNgramField(model_attr='title')
     category = EdgeNgramField(model_attr='category')
+    url= CharField()
     
+    def prepare_url(self,obj):
+        try:
+            return "#events/%s/tab/%s" %(slugify(obj.title),slugify(obj.tab_set.all()[0].title))
+        except:
+            return "#events/%s/tab" %slugify(obj.title)
+               
 site.register(Tab, NoteIndexTabs)
 site.register(Event, NoteIndexEvents)
 
