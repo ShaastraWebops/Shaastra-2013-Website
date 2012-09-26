46dbf6a0243831e9f193e3e005c34f43093d60fc
Anant Girdhar <anant.girdhar@gmail.com>
2012-09-26 22:31:30 +0530

    Event registration: Change order of URLs.

--- events/urls.py
+++ events/urls.py
@@ -8,9 +8,9 @@ from events.views import *
 
 urlpatterns = patterns('',
                        url(r'^sponslogo', logo, name='spons-logo'),
+                       url(r'^(?P<event_id>\d+)/register/$', register),
                        url(r'^(?P<event_name>.+)/tab/(?P<tab_name>.+)', tabs, name='tab-list'),
                        url(r'^(?P<event_name>.+)', events, name='event-list'),
                        url(r'^sampark/', sampark, name='sampark-home'),
                        url(r'^$', home, name='event-home'),
-                       url(r'^(?P<event_id>\d+)/register/$', register),
                        )
